d028b4eaece1f6bd46568a1757704680456d714b
Don Blair <donblair>
2013-06-03 16:18:11 -0400

    fixed letterboxing

--- app/views.py
+++ app/views.py
@@ -21,16 +21,13 @@ from werkzeug import secure_filename
 
 import matplotlib
 matplotlib.use('Agg')
-from matplotlib import pyplot
 #import numpy
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as numpy
 
 from PIL import Image
-
-import flask_sijax
-
+import gc
 
 ### in order to handle odd filenames
 # see online material here: http://flask.pocoo.org/snippets/5/
@@ -59,6 +56,7 @@ def favicon():
 
 ### generating ndvi
 
+"""
 def nir(imageInPath,imageOutPath):
     img = mpimg.imread(imageInPath)
     red=img[:,:,0]
@@ -76,8 +74,55 @@ def nir(imageInPath,imageOutPath):
 
     #fig.colorbar(nir_plot)
     fig.savefig(imageOutPath)
+"""
 
- 
+def nir(imageInPath,imageOutPath):
+	img=Image.open(imageInPath)
+	imgR, imgG, imgB = img.split() #get channels
+	arrR = numpy.asarray(imgR).astype('float64')
+
+	arr_nir=arrR
+
+	img_w,img_h=img.size
+
+	dpi=600. #need this to be floating point!
+	fig_w=img_w/dpi
+	fig_h=img_h/dpi
+
+	fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
+
+	fig.set_frameon(False)
+
+	ax_rect = [0.0, #left
+	       0.0, #bottom
+	       1.0, #width
+	       1.0] #height
+	ax = fig.add_axes(ax_rect)
+	ax.yaxis.set_ticklabels([])
+	ax.xaxis.set_ticklabels([])   
+	ax.set_axis_off()
+	ax.axes.get_yaxis().set_visible(False)
+	ax.patch.set_alpha(0.0)
+
+	axes_img = ax.imshow(arr_nir,
+			  cmap=plt.cm.gist_gray,
+			  aspect = 'equal',
+			  interpolation="nearest"
+			 )
+
+	fig.savefig(imageOutPath, 
+		    dpi=dpi,
+		    bbox_inches='tight',
+		    pad_inches=0.0, 
+		   )
+
+	#plt.show()  #show the plot after saving
+	fig.clf()
+	plt.close()
+	gc.collect()
+
+
+
 def ndvi(imageInPath,imageOutPath):
 	img=Image.open(imageInPath)
 	imgR, imgG, imgB = img.split() #get channels
@@ -97,6 +142,115 @@ def ndvi(imageInPath,imageOutPath):
 	denom=(arrR + arrB)
 	arr_ndvi=num/denom
 
+	vmin=-1.
+	vmax = 1.
+
+	img_w,img_h=img.size
+
+	dpi=600. #need this to be floating point!
+	fig_w=img_w/dpi
+	fig_h=img_h/dpi
+
+	fig=plt.figure(figsize=(fig_w,fig_h),dpi=dpi)
+
+	fig.set_frameon(False)
+
+	ax_rect = [0.0, #left
+	       0.0, #bottom
+	       1.0, #width
+	       1.0] #height
+	ax = fig.add_axes(ax_rect)
+	ax.yaxis.set_ticklabels([])
+	ax.xaxis.set_ticklabels([])   
+	ax.set_axis_off()
+	ax.axes.get_yaxis().set_visible(False)
+	ax.patch.set_alpha(0.0)
+
+
+	"""
+	axes_img = ax.imshow(arr_ndvi,
+	cmap=plt.cm.spectral, 
+	vmin = vmin,
+	vmax = vmax,
+	aspect = 'equal',
+	interpolation="nearest"
+	)
+	"""
+
+	axes_img = ax.imshow(arr_ndvi,
+			  cmap=plt.cm.spectral, 
+			  aspect = 'equal',
+			  interpolation="nearest"
+			 )
+
+	# Add colorbar 
+	#make an axis for colorbar
+	cax = fig.add_axes([0.8,0.05,0.05,0.85]) #left, bottom, width, height
+	cbar = fig.colorbar(axes_img, cax=cax)  #this resizes the axis
+	cbar.ax.tick_params(labelsize=2) #this changes the font size on the axis
+
+	#position of the colorbar
+	#cbar.ax.yaxis.set_ticks_position('left')
+
+	#color of the colorbar text
+	#cbytick_obj = plt.getp(cbar.ax.axes, 'yticklabels')                #tricky
+	#plt.setp(cbytick_obj, color='r')
+
+
+	fig.savefig(imageOutPath, 
+		    dpi=dpi,
+		    bbox_inches='tight',
+		    pad_inches=0.0, 
+		   )
+
+	#plt.show()  #show the plot after saving
+	fig.clf()
+	plt.close()
+	gc.collect()
+
+
+
+"""
+	if histogramOption==1:
+
+	#plot the Red histogram
+	x=arrR.ravel()
+	a = plt.axes([.05,.7,.18,.18], axisbg='y')
+	bins=numpy.arange(0,255,8)
+	n, bins, patches = plt.hist(x, bins, normed=1,linewidth=.2)
+	plt.setp(patches, 'facecolor', 'r', 'alpha', 0.75)
+	plt.setp(a,xticks=[0,120,255],yticks=[])
+	plt.setp(a,xticks=[],yticks=[])
+	plt.xticks(fontsize=2)
+
+	#plot the Blue histogram
+	x=arrB.ravel()
+	a = plt.axes([.05,.4,.18,.18], axisbg='y')
+	bins=numpy.arange(0,255,8)
+	n, bins, patches = plt.hist(x, bins, normed=1,linewidth=.2)
+	plt.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
+	plt.setp(a,xticks=[0,120,255],yticks=[])
+	plt.setp(a,xticks=[],yticks=[])
+	plt.xticks(fontsize=2)
+
+	#plot the NDVI histogram
+	x=arr_ndvi.ravel()
+	a = plt.axes([.05,.1,.18,.18], axisbg='y')
+	bins=numpy.arange(-1,1,.01)
+	n, bins, patches = plt.hist(x, bins, normed=1,linewidth=.2)
+	plt.setp(patches, 'facecolor', 'w', 'alpha', 0.75)
+	plt.setp(a,xticks=[-1,0,1],yticks=[])
+	plt.setp(a,xticks=[],yticks=[])
+	plt.xticks(fontsize=2)
+"""
+
+
+
+
+
+
+"""
+
 	fig=plt.figure()
 	fig.set_frameon(False)
 	ax=fig.add_subplot(111)
@@ -109,6 +263,14 @@ def ndvi(imageInPath,imageOutPath):
 
 	fig.colorbar(ndvi_plot)
 	fig.savefig(imageOutPath)
+"""
+
+
+
+
+
+
+
 
 def allowed_file(filename):
     return '.' in filename and \
